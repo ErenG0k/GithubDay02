b2dd668847a2b907970aaa58efbda0288aa416b4
Eren Gok <mehmeterengok66@gmail.com>
2023-08-01 15:15:15 +0300

    ikinci ucuncu satirlar eklendi

--- src/github02.java
+++ src/github02.java
@@ -2,5 +2,7 @@ public class github02 {
     public static void main(String[] args) {
 
         System.out.println("hello github02");
+        System.out.println("github");
+        System.out.println("Java||github");
     }
 }
